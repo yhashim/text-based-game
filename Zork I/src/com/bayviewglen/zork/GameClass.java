cc332d1d42fb4a33a2074fb4ac05220b52f0ed8d
yhashim <36962808+yhashim@users.noreply.github.com>
2019-05-08 14:56:40 -0400

    rooms.dat file prepared to be filled out, fixed game start error

--- Zork I/src/com/bayviewglen/zork/GameClass.java
+++ Zork I/src/com/bayviewglen/zork/GameClass.java
@@ -91,7 +91,7 @@ class Game {
 	public Game() {
 		try {
 			initRooms("data/Rooms.dat");
-			currentRoom = masterRoomMap.get("ROOM_1");
+			currentRoom = masterRoomMap.get("CLASSROOM");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
