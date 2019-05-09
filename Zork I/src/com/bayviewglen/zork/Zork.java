47a8a0648d7df5d4e0b257c58335644634eafc20
yhashim <36962808+yhashim@users.noreply.github.com>
2019-05-09 12:16:00 -0400

    commit

--- Zork I/src/com/bayviewglen/zork/Zork.java
+++ Zork I/src/com/bayviewglen/zork/Zork.java
@@ -15,17 +15,46 @@ public class Zork {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		print("You are sitting in the classroom. There is a large window to your right.\r\nThe teacher is writing something on the chalkboard. \r\n" , lapse);
+		print("You are sitting in the classroom. There is a large window to your right.\r\nThe teacher is writing something on the chalkboard. \r\n",
+				lapse);
 		try {
 			TimeUnit.MILLISECONDS.sleep(250);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		print("Glancing outside the window, you can see the side of the school.\r\nA black blur suddenly falls from the sky.\r\n" , lapse);
-
+		print("Glancing outside the window, you can see the side of the school.\r\nA black blur suddenly falls from the sky.\r\n",
+				lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("The bell rings and you exit the classroom. \r\n", lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("...\r\n", lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("You are standing at the front of the school. To the \r\n"
+				+ "west is the side of the school. Green Odori St. is south. \r\n", lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// Type 'help' if you need help.
-		// Game game = new Game(); 
+		// Game game = new Game();
 		// game.play();
 	}
 
