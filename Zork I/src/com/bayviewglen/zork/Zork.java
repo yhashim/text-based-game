3fede3c44b66f8254d8b739c7959a3777fa76a83
yhashim <36962808+yhashim@users.noreply.github.com>
2019-05-10 12:06:31 -0400

    updated pre-story text

--- Zork I/src/com/bayviewglen/zork/Zork.java
+++ Zork I/src/com/bayviewglen/zork/Zork.java
@@ -45,14 +45,37 @@ public class Zork {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		print("You are standing at the front of the school. To the \r\n"
-				+ "west is the side of the school. Green Odori St. is south. \r\n", lapse);
+		print("You are standing at the front of the school. To the west is the side of the school. Green Odori St. is south. \r\n",
+				lapse);
 		try {
 			TimeUnit.MILLISECONDS.sleep(250);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		print("You walk west, towards the field where you saw the black blur fall. \r\n", lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("...\r\n", lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("You are standing in a field of grass at the side of the school.\r\nThere is an apple tree in the center of the field. A mysterious black object lays next to it. \r\n",
+				lapse);
+		try {
+			TimeUnit.MILLISECONDS.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("You walk towards the tree. Upon closer look, you are able to see that the object is a black notebook titled \"Death Note\".", lapse);
 		// Type 'help' if you need help.
 		// Game game = new Game();
 		// game.play();
