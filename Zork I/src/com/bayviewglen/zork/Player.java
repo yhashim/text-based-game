d794e2c4275c55603cae0a7d4d35c733b1b8a055
kchong2 <kchong2@kchongsp4.bayviewglen.ca>
2019-05-28 07:58:18 -0400

    initial player class push - includes inventory

---
+++ Zork I/src/com/bayviewglen/zork/Player.java
@@ -0,0 +1,54 @@
+package com.bayviewglen.zork;
+import java.util.HashMap;
+
+public class Player {
+	private HashMap <String, Item> items = new HashMap <String, Item>();
+	
+	// adds an item to the inventory
+	// if an item is already 
+	public void addToInventory(Item item, int amount) {
+		if (items.containsKey(item.getItemName())) {
+			items.get(item.getItemName()).addAmount(amount);
+		}
+		else {
+			items.put(item.getItemName(), item);
+		}
+	}
+	
+	/*
+	// checks for an item in the inventory 
+	// return true if it is, false if not
+	public boolean checkItem(Item item) {
+		return items.containsValue(item);
+	}
+	*/
+	
+	public String toString() {
+		String inv = "";
+		for (Item i: items.values()) {
+			inv += "\n " + i;
+		}
+		if (inv.length() <= 0) {
+			return "There is nothing in your inventory";
+		}
+		return inv;
+	}
+	
+	public void removeItem(String name, int amount) {
+		if (items.get(name).getAmount() == 1) {
+			items.remove(name);
+		}
+		else {
+			items.get(name).setAmount(items.get(name).getAmount()-1); 
+		}
+	}
+	
+	
+	
+	
+	
+
+	
+	
+}
+
