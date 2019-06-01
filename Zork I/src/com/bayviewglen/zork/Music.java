9653d2c03680c6dfd18559e388f432b91cb83f8f
ksharma2 <ksharma2@ksharmasp4.bayviewglen.ca>
2019-05-31 22:11:02 -0400

    added code to stop and start the music

--- Zork I/src/com/bayviewglen/zork/Music.java
+++ Zork I/src/com/bayviewglen/zork/Music.java
@@ -5,6 +5,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.JOptionPane;
 
 public class Music{
@@ -21,8 +22,8 @@ public class Music{
 				// brings the music from the file to eclipse 
 				Clip clip = AudioSystem.getClip();
 				clip.open(audioInput);
-				clip.start();
-				clip.loop(Clip.LOOP_CONTINUOUSLY);
+				//clip.start();
+				//clip.loop(Clip.LOOP_CONTINUOUSLY);
 				
 				JOptionPane.showMessageDialog(null, "Press OK to stop playing");
 			}else {
@@ -34,87 +35,19 @@ public class Music{
 			
 			ex.printStackTrace();
 		}
+		
 	}
-}
-
-
-
-
-
-
-/*
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javazoom.jl.player.Player;
-
-import sun.audio.*;
-
-
-
-
-public class Music {
-	
-	 // play the MP3 file to the sound card
-    public static void play() {
-    	String filename = "C:\\Users\\ksharma2\\git\\Zork\\Music";
-    	try {
-        	
-            FileInputStream fis     = new FileInputStream(filename);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            Player player = new Player(bis);
-            player.play();
-           
-        }
-        catch (Exception e) {
-            System.out.println("Problem playing file " + filename);
-            System.out.println(e);
-        }
+	public void play(DataLine clip){ //plays the music without looping
+        clip.start();
     }
-
-	public static void main(String[] args) {
-		
-		play();
-	}
+    public void loop(DataLine clip){ //loops the music
+        ((Clip) clip).loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public static void stop(DataLine clip){ //stops the music 
+        clip.close();
+    }
+   
+	
 }
 
-
-
-/*
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import javax.swing.JOptionPane;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-
-
-public class Music {
-	
-	public static void main (String[] args) {
-		
-		play("C:\\Users\\ksharma2\\git\\Zork\\Music");
-	}
-	
-   public static void play(String filepath) {
-	   InputStream music;
-	   try {
-		   music = new FileInputStream(new File(filepath));
-		   AudioStream audios = new AudioStream(music);
-		   AudioPlayer.player.start(audios);
-	   }
-	   catch(Exception e) {
-		   JOptionPane.showMessageDialog(null,"Error");
-		   
-	   }
-	     
-	   
-   }
-  
-}
-*/
 
