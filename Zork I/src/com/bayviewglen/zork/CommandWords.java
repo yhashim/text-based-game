f65b49a8ff8c295d3e52c6bb32ac716c43ba668f
yhashim <36962808+yhashim@users.noreply.github.com>
2019-06-01 03:01:16 -0400

    Changed pick up to seize, as the "up" part was confused with direction

--- Zork I/src/com/bayviewglen/zork/CommandWords.java
+++ Zork I/src/com/bayviewglen/zork/CommandWords.java
@@ -13,7 +13,7 @@ package com.bayviewglen.zork;
 class CommandWords {
 	// a constant array that holds all valid command words
 	private static final String validCommands[] = { "go", "walk", "proceed", "run", "write", "kill", "use", "read",
-			"take", "pick up", "examine", "look", "watch", "unlock", "open", "give", "hand", "drop", "put down",
+			"take", "seize", "examine", "look", "watch", "unlock", "open", "give", "hand", "drop", "put down",
 			"leave", "quit", "finish", "retire", "help", "eat", "consume" };
 	// ...
 
