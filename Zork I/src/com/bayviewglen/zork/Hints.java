64f7315cb0ee48e0b2b5f526a26fa3483b3a1eb8
ksharma2 <ksharma2@ksharmasp4.bayviewglen.ca>
2019-06-01 00:10:53 -0400

    changed location of music files

---
+++ Zork I/src/com/bayviewglen/zork/Hints.java
@@ -0,0 +1,13 @@
+package com.bayviewglen.zork;
+
+public class Hints {
+public String getHint(){
+	/*
+	 * if # people killed < 5
+	 * generate random hint from first hints 
+	 * if # people killed >= 5
+	 * generate random hint from second hints 
+	 */
+	
+}
+}
