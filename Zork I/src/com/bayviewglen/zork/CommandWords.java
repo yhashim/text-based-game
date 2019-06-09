fbd9c495bc162ff0fd37220feb217925d14fd7a6
yhashim <36962808+yhashim@users.noreply.github.com>
2019-06-09 15:06:02 -0400

    Formatted (not much to fix)

--- Zork I/src/com/bayviewglen/zork/CommandWords.java
+++ Zork I/src/com/bayviewglen/zork/CommandWords.java
@@ -6,19 +6,19 @@ package com.bayviewglen.zork;
  * Date:    July 1999
  * 
  * This class holds an enumeration of all command words known to the game.
- * It is used to recognise commands as they are typed in.
+ * It is used to recognize commands as they are typed in.
  *
  * This class is part of the "Zork" game.
  */
+
 class CommandWords {
 	// a constant array that holds all valid command words
-	private static final String validCommands[] = { "go", "walk", "listen", "proceed", "run", "write", "kill", "use", "read",
-			"take", "seize", "examine", "look", "watch", "unlock", "open", "give", "hand", "drop", "put down",
+	private static final String validCommands[] = { "go", "walk", "listen", "proceed", "run", "write", "kill", "use",
+			"read", "take", "seize", "examine", "look", "watch", "unlock", "open", "give", "hand", "drop", "put down",
 			"leave", "quit", "finish", "retire", "help", "eat", "consume", "activate" };
-	// ...
 
 	/**
-	 * Constructor - initialise the command words.
+	 * Constructor - initialize the command words.
 	 */
 	public CommandWords() {
 		// nothing to do at the moment...
@@ -31,7 +31,7 @@ class CommandWords {
 	public boolean isCommand(String aString) {
 		for (int i = 0; i < validCommands.length; i++) {
 			if (validCommands[i].equals(aString))
-				return true; 
+				return true;
 		}
 		// if we get here, the string was not found in the commands...
 		return false;
